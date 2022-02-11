f315af173627ca0fc14fd493fd8dd25fb5a2f3b3
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-02-11 15:46:04 -0300

    Consulta simples com @Query

--- src/main/java/com/silvaronaldo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/silvaronaldo/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,19 @@ package com.silvaronaldo.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.silvaronaldo.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+
+	//{ <field>: { $regex: /pattern/, $options: '<options>' } }
+	//i ignoreCase
+	//?0 primeiro parametro, no caso String text
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	
 	//só com isso já faz a busca, query methods
