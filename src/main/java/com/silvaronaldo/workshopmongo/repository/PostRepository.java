9e2b998ee90d10feddccb58d4f7d03d752caa750
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-02-11 15:34:30 -0300

    Consulta simples com query methods

--- src/main/java/com/silvaronaldo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/silvaronaldo/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.silvaronaldo.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.silvaronaldo.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	
+	//só com isso já faz a busca, query methods
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/silvaronaldo/workshopmongo/resource/PostResource.java
+++ src/main/java/com/silvaronaldo/workshopmongo/resource/PostResource.java
@@ -1,23 +1,25 @@
 package com.silvaronaldo.workshopmongo.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.silvaronaldo.workshopmongo.domain.Post;
+import com.silvaronaldo.workshopmongo.resource.util.URL;
 import com.silvaronaldo.workshopmongo.services.PostService;
 
 @RestController
 @RequestMapping(value = "/posts")
-public class PostUserResource {
+public class PostResource {
 
 	@Autowired
 	private PostService service;
-
-
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id) {	
@@ -25,5 +27,14 @@ public class PostUserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	//como será a busca na url
+	//http://localhost:8080/posts/titlesearch?text=bom%20dia	
+	
+	@RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParams(text); //codificar o texto, qdo é enviado está codificado
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 		
 }
