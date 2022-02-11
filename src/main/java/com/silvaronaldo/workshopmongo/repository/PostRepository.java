5506983530f7f07a2743683d8b99cc8473df56c7
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-02-11 14:04:19 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/silvaronaldo/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.silvaronaldo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.silvaronaldo.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
