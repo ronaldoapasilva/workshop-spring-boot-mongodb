4247ddab8b5f9c9cb3ae7d0e5be2b6dfed3f173d
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-02-11 14:10:57 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/silvaronaldo/workshopmongo/domain/Post.java
+++ src/main/java/com/silvaronaldo/workshopmongo/domain/Post.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import com.silvaronaldo.workshopmongo.dto.AuthorDTO;
+
 public class Post implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -11,7 +13,7 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	
 	public Post() {
@@ -19,7 +21,7 @@ public class Post implements Serializable {
 	}
 
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -68,12 +70,12 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 	
